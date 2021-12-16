7b1eb21e0f5f6855331def358dc2d6954c47912d
codu2 <82283517+codu2@users.noreply.github.com>
2021-12-16 22:49:40 +0900

    Update cart-slice.js

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -4,14 +4,20 @@ const cartSlice = createSlice({
     name: 'cart',
     initialState: {
         items: [],
-        totalQuantity: 0
+        totalQuantity: 0,
+        changed: false
     },
     reducers: {
+        replaceCart(state, action) {
+            state.totalQuantity = action.payload.totalQuantity;
+            state.items = action.payload.items;
+        },
         addItemToCart(state, action) {
             const newItem = action.payload;
             const existingItem = state.items.find((item) => item.id === newItem.id);
             //cart에 newItem이 이미 존재하는지 확인하는 방법
             state.totalQuantity++;
+            state.changed = true;
             if(!existingItem) {
                 state.items.push({
                     id: newItem.id,
@@ -30,6 +36,7 @@ const cartSlice = createSlice({
             const id = action.payload;
             const existingItem = state.items.find(item => item.id === id);
             state.totalQuantity--;
+            state.changed = true;
             if(existingItem.quantity === 1) {
                 state.items = state.items.filter(item => item.id !== id);
             } else {
